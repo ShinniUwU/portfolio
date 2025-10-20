5ccc033baf3c1faf882d84998aca1a80e761d8ae
ShinniUwU <shinni@tutamail.com>
2025-10-20 22:07:41 +0300

    fix robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next"
 
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
@@ -9,4 +11,3 @@ export default function robots(): MetadataRoute.Robots {
     sitemap: "https://shinni.dev/sitemap.xml",
   }
 }
-
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next"
 
+export const dynamic = "force-static"
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://shinni.dev"
   return [
@@ -11,4 +13,3 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
   ]
 }
-
