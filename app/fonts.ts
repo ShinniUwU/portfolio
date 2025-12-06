68eef42ed72ca7c31037c742481823125df753ad
ShinniUwU <shinni@tutamail.com>
2025-12-06 18:31:06 +0200

    redesign

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,5 +1,4 @@
-import { Inter, JetBrains_Mono } from "next/font/google"
+import { Space_Grotesk, JetBrains_Mono } from "next/font/google"
 
-export const fontSans = Inter({ subsets: ["latin"] })
+export const fontSans = Space_Grotesk({ subsets: ["latin"], weight: ["400","500","600","700"] })
 export const fontMono = JetBrains_Mono({ subsets: ["latin"], weight: ["400","500","700"] })
-
